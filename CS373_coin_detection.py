da21d5b05bd15ba4eed31ec36e4834a6c28b0812
Jinhua <228871192@qq.com>
2024-05-18 02:24:02 +1000

    updated coin_detection_extension added two jupyter notebooks

--- CS373_coin_detection.py
+++ CS373_coin_detection.py
@@ -1,8 +1,6 @@
 # Built in packages
 import sys
-import cv2
 import CS373_coin_detection_extension as cs373
-from matplotlib import pyplot as plt
 
 # Matplotlib will need to be installed if it isn't already. This is the only package allowed for this base part of the
 # assignment.
@@ -72,73 +70,6 @@ def createInitializedGreyscalePixelArray(image_width, image_height, initValue=0)
 ###########################################
 
 
-def plot_task_1(original_image, stretched_image):
-    fig, axs = pyplot.subplots(1, 2)
-    axs[0].imshow(original_image, cmap='gray')
-    axs[0].set_title('Original Image')
-    axs[0].axis('off')
-    axs[1].imshow(stretched_image, cmap='gray')
-    axs[1].set_title('Stretched Image')
-    axs[1].axis('off')
-    fig.suptitle('Convert to greyscale and normalize')
-
-def plot_task_2(scharr_x, scharr_y, abs_diff):
-    # Plot scharr_x and scharr_y mapping
-    fig, axs = pyplot.subplots(1, 3)
-    axs[0].imshow(scharr_x, cmap='gray')
-    axs[0].set_title('scharr x-direction')
-    axs[0].axis('off')
-    axs[1].imshow(scharr_y, cmap='gray')
-    axs[1].set_title('scharr y-direction')
-    axs[1].axis('off')
-
-    # Plot absolute different
-    axs[2].imshow(abs_diff, cmap='gray')
-    axs[2].set_title('abs different')
-    axs[2].axis('off')
-    fig.suptitle('Edge detection')
-
-def plot_task_3(blurred_image_1, blurred_image_2):
-    blurred_image_1 = np.array(blurred_image_1)
-    blurred_image_2 = np.array(blurred_image_2)
-    # apply gaussian fill
-    fig, axs = pyplot.subplots(2, 2)
-    axs[0][0].imshow(blurred_image_1, cmap='gray')
-    axs[0][0].set_title('One time')
-    axs[0][0].axis('off')
-    axs[0][1].hist(blurred_image_1.ravel(), 256, [0, 255])
-    axs[0][1].set_title('Histogram')
-
-    axs[1][0].imshow(blurred_image_2, cmap='gray')
-    axs[1][0].set_title('Two times')
-    axs[1][0].axis('off')
-    axs[1][1].hist(blurred_image_2.ravel(), 256, [0, 255])
-
-    fig.suptitle('Applying Gaussian filter')
-
-def plot_task_4(blurred_image_2, binary_image):
-    fig, axs = pyplot.subplots(1, 2)
-    axs[0].imshow(blurred_image_2, cmap='gray')
-    axs[0].set_title('blurred_image_2')
-    axs[0].axis('off')
-    axs[1].imshow(binary_image, cmap='gray')
-    axs[1].set_title('binary_image')
-    axs[1].axis('off')
-    fig.suptitle('Threshold the Image')
-
-def polt_task_5(dilation_2, dilation_3, dilation_4):
-    fig, axs = pyplot.subplots(1, 3)
-    axs[0].imshow(dilation_2, cmap='gray')
-    axs[0].set_title('dilation_2')
-    axs[0].axis('off')
-    axs[1].imshow(dilation_3, cmap='gray')
-    axs[1].set_title('dilation_3')
-    axs[1].axis('off')
-    axs[2].imshow(dilation_4, cmap='gray')
-    axs[2].set_title('dilation_4')
-    axs[2].axis('off')
-    fig.suptitle('Erosion and Dilation')
-
 
 # This is our code skeleton that performs the coin detection.
 def main(input_path, output_path):
@@ -150,58 +81,68 @@ def main(input_path, output_path):
 
     # we read in the png file, and receive three pixel arrays for red, green and blue components, respectively
     # each pixel array contains 8 bit integer values between 0 and 255 encoding the color values
+    print("RGB to Grey")
     (image_width, image_height, px_array_r, px_array_g, px_array_b) = readRGBImageToSeparatePixelArrays(input_filename)
 
     ###################################
     ### STUDENT IMPLEMENTATION Here ###
     ###################################
-    original_image = np.stack((px_array_r, px_array_g, px_array_b), axis=-1)
+
 
     # Task 1 Convert to greyscale and normalize
-    gray_scale_image = cs373.convert_RGB_to_Grayscale(px_array_r, px_array_g, px_array_b, image_width, image_height)
-    stretched_image = cs373.percentile_based_mapping(gray_scale_image, image_width, image_height)
-    plot_task_1(original_image, stretched_image)
+    print("Task 1")
+    grey_scale_image = cs373.convert_RGB_to_Grayscale(px_array_r, px_array_g, px_array_b, image_width, image_height)
+    stretched_image = cs373.percentile_based_mapping(grey_scale_image, image_width, image_height)
+    cs373.plot_task_1(grey_scale_image, stretched_image)
 
     # Task 2 Edge detection
     # Find both scharr_x and scharr_y mapping
+    print("Task 2")
     scharr_x = [[-3, 0, 3], [-10, 0, 10], [-3, 0, 3]]
     scharr_y = [[-3, -10, -3], [0, 0, 0], [3, 10, 3]]
 
-    scharr_x = cs373.apply_3x3_filter(stretched_image, image_width, image_height,  scharr_x)
-    scharr_y = cs373.apply_3x3_filter(stretched_image, image_width, image_height,  scharr_y)
+    scharr_x = cs373.apply_filter(stretched_image, image_width, image_height,  scharr_x)
+    scharr_y = cs373.apply_filter(stretched_image, image_width, image_height,  scharr_y)
     abs_diff = cs373.abs_diff(scharr_x, scharr_y)
-    plot_task_2(scharr_x, scharr_y, abs_diff)
+    cs373.plot_task_2(scharr_x, scharr_y, abs_diff)
 
     # Task 3 Image blurring
-    mean_filter_5x5 = [[1/5 for i in range(5)] for j in range(5)]
-    print(mean_filter_5x5)
-    blurred_image_1 = cs373.apply_5x5_filter(abs_diff, image_width, image_height, mean_filter_5x5)
-    blurred_image_2 = cs373.apply_5x5_filter(blurred_image_1, image_width, image_height, mean_filter_5x5)
+    print("Task 3")
+    mean_filter_5x5 = [[0.04, 0.04, 0.04, 0.04, 0.04],
+                       [0.04, 0.04, 0.04, 0.04, 0.04],
+                       [0.04, 0.04, 0.04, 0.04, 0.04],
+                       [0.04, 0.04, 0.04, 0.04, 0.04],
+                       [0.04, 0.04, 0.04, 0.04, 0.04]]
 
+    blurred_image_1 = cs373.apply_filter(abs_diff, image_width, image_height, mean_filter_5x5)
+    blurred_image_2 = cs373.apply_filter(blurred_image_1, image_width, image_height, mean_filter_5x5)
+    blurred_image_2 = cs373.apply_filter(blurred_image_2, image_width, image_height, mean_filter_5x5)
+    cs373.plot_task_3(blurred_image_1, blurred_image_2)
 
-    plot_task_3(blurred_image_1, blurred_image_2)
 
     # Task 4 Threshold the Image
-    threshold_value = 250  # Based on the histogram
-    binary_image = cs373.custom_threshold(blurred_image_2, image_width, image_height, 50, 200)
-    plot_task_4(blurred_image_2, binary_image)
+    print("Task 4")
+    binary_image = cs373.custom_threshold(blurred_image_1, image_width, image_height, 180, 200)
+    cs373.plot_task_4(blurred_image_2, binary_image)
 
     # Task 5 Erosion and Dilation
-    circular_kernel = np.array([[0, 0, 1, 0, 0],
-                                [0, 1, 1, 1, 0],
-                                [1, 1, 1, 1, 1],
-                                [0, 1, 1, 1, 0],
-                                [0, 0, 1, 0, 0]], dtype=np.uint8)
+    print("Task 5")
+    circular_kernel = [[0, 0, 1, 0, 0],
+                       [0, 1, 1, 1, 0],
+                       [1, 1, 1, 1, 1],
+                       [0, 1, 1, 1, 0],
+                       [0, 0, 1, 0, 0]]
 
-    # dilation_2 = cv2.dilate(binary_image, circular_kernel, iterations=2)
-    # dilation_3 = cv2.dilate(binary_image, circular_kernel, iterations=3)
-    # dilation_4 = cv2.dilate(binary_image, circular_kernel, iterations=4)
-    # polt_task_5(dilation_2, dilation_3, dilation_4)
+    dilated_image = cs373.dilate_image(binary_image, image_width, image_height, circular_kernel, iterations=2)
+    eroded_image = cs373.erode_image(dilated_image, image_width, image_height, circular_kernel, iterations=4)
+    cs373.polt_task_5(binary_image, dilated_image, eroded_image)
 
     # Task 6 Connected Component Analysis
-    regions = cs373.computeConnectedComponentLabeling(binary_image, image_width, image_height)
+    print("Task 6")
+    regions = cs373.computeConnectedComponentLabeling(eroded_image, image_width, image_height)
 
     # Task 7 Draw Bounding Box
+    print("Task 7")
     bounding_box_list = cs373.detect_the_region(regions)
 
     ############################################
--- CS373_coin_detection_extension.py
+++ CS373_coin_detection_extension.py
@@ -1,4 +1,74 @@
-import numpy as np
+from matplotlib import pyplot
+import imageIO
+
+# Plot
+def plot_task_1(original_image, stretched_image):
+    fig, axs = pyplot.subplots(1, 2)
+    axs[0].imshow(original_image, cmap='gray')
+    axs[0].set_title('Original Image')
+    axs[0].axis('off')
+    axs[1].imshow(stretched_image, cmap='gray')
+    axs[1].set_title('Stretched Image')
+    axs[1].axis('off')
+    fig.suptitle('Convert to greyscale and normalize')
+
+def plot_task_2(scharr_x, scharr_y, abs_diff):
+    # Plot scharr_x and scharr_y mapping
+    fig, axs = pyplot.subplots(1, 3)
+    axs[0].imshow(scharr_x, cmap='gray')
+    axs[0].set_title('scharr x-direction')
+    axs[0].axis('off')
+    axs[1].imshow(scharr_y, cmap='gray')
+    axs[1].set_title('scharr y-direction')
+    axs[1].axis('off')
+
+    # Plot absolute different
+    axs[2].imshow(abs_diff, cmap='gray')
+    axs[2].set_title('abs different')
+    axs[2].axis('off')
+    fig.suptitle('Edge detection')
+
+def plot_task_3(blurred_image_1, blurred_image_2):
+    flat_list_1 = [item for sublist in blurred_image_1 for item in sublist]
+    flat_list_2 = [item for sublist in blurred_image_2 for item in sublist]
+    # apply gaussian fill
+    fig, axs = pyplot.subplots(2, 2)
+    axs[0][0].imshow(blurred_image_1, cmap='gray')
+    axs[0][0].set_title('One time')
+    axs[0][0].axis('off')
+    axs[0][1].hist(flat_list_1, 256, range=(0, 255))
+    axs[0][1].set_title('Histogram')
+
+    axs[1][0].imshow(blurred_image_2, cmap='gray')
+    axs[1][0].set_title('Two times')
+    axs[1][0].axis('off')
+    axs[1][1].hist(flat_list_2, 256, range=(0, 255))
+
+    fig.suptitle('Applying mean filter')
+
+def plot_task_4(blurred_image_2, binary_image):
+    fig, axs = pyplot.subplots(1, 2)
+    axs[0].imshow(blurred_image_2, cmap='gray')
+    axs[0].set_title('blurred_image')
+    axs[0].axis('off')
+    axs[1].imshow(binary_image, cmap='gray')
+    axs[1].set_title('binary_image')
+    axs[1].axis('off')
+    fig.suptitle('Threshold the Image')
+
+def polt_task_5(binary_image, dilated_image, eroded_image):
+    fig, axs = pyplot.subplots(1, 3)
+    axs[0].imshow(binary_image, cmap='gray')
+    axs[0].set_title('binary_image')
+    axs[0].axis('off')
+    axs[1].imshow(dilated_image, cmap='gray')
+    axs[1].set_title('dilated_image')
+    axs[1].axis('off')
+    axs[2].imshow(eroded_image, cmap='gray')
+    axs[2].set_title('eroded_image')
+    axs[2].axis('off')
+    fig.suptitle('Dilation and Erosion ')
+
 
 
 def createInitializedGreyscalePixelArray(image_width, image_height, initValue=0):
@@ -10,6 +80,42 @@ def createInitializedGreyscalePixelArray(image_width, image_height, initValue=0)
         new_pixel_array.append(new_row)
 
     return new_pixel_array
+def readRGBImageToSeparatePixelArrays(input_filename):
+    image_reader = imageIO.png.Reader(filename=input_filename)
+    # png reader gives us width and height, as well as RGB data in image_rows (a list of rows of RGB triplets)
+    (image_width, image_height, rgb_image_rows, rgb_image_info) = image_reader.read()
+
+    print("read image width={}, height={}".format(image_width, image_height))
+
+    # our pixel arrays are lists of lists, where each inner list stores one row of greyscale pixels
+    pixel_array_r = []
+    pixel_array_g = []
+    pixel_array_b = []
+
+    for row in rgb_image_rows:
+        pixel_row_r = []
+        pixel_row_g = []
+        pixel_row_b = []
+        r = 0
+        g = 0
+        b = 0
+        for elem in range(len(row)):
+            # RGB triplets are stored consecutively in image_rows
+            if elem % 3 == 0:
+                r = row[elem]
+            elif elem % 3 == 1:
+                g = row[elem]
+            else:
+                b = row[elem]
+                pixel_row_r.append(r)
+                pixel_row_g.append(g)
+                pixel_row_b.append(b)
+
+        pixel_array_r.append(pixel_row_r)
+        pixel_array_g.append(pixel_row_g)
+        pixel_array_b.append(pixel_row_b)
+
+    return (image_width, image_height, pixel_array_r, pixel_array_g, pixel_array_b)
 
 
 
@@ -31,23 +137,23 @@ def convert_RGB_to_Grayscale(pixel_array_r, pixel_array_g, pixel_array_b, image_
 
 
 # Using 5-95 percentile strategy
-def get_percentiles(pixel_array):
-    # Convert pixel_array into numpy array
-    pixel_array = np.array(pixel_array)
-
-    # Count percentiles
-    percentiles = {
-        '5th': np.percentile(pixel_array, 5),
-        '95th': np.percentile(pixel_array, 95)
-    }
-
-    return percentiles
-
+def percentile(pixel_array, percentile):
+    # 将数据转换为列表（如果不是）并排序
+    pixel_array = [pixel_array[row][col] for row in range(len(pixel_array)) for col in range(len(pixel_array[0]))]
+    size = len(pixel_array)
+    sorted_data = sorted(pixel_array)
+    # 计算百分位数位置
+    index = size * percentile // 100
+    # 根据百分位数返回相应的数据值
+    return sorted_data[index]
 
 def percentile_based_mapping(pixel_array, image_width, image_height):
     contrast_stretched_pixel_array = createInitializedGreyscalePixelArray(image_width, image_height)
 
-    percentiles = get_percentiles(pixel_array)
+    percentiles = {
+        '5th': percentile(pixel_array, 5),
+        '95th': percentile(pixel_array, 95)
+    }
 
     for row in range(image_height):
         for col in range(image_width):
@@ -57,17 +163,18 @@ def percentile_based_mapping(pixel_array, image_width, image_height):
     return contrast_stretched_pixel_array
 
 
-
 # Task 2 Edge detection
-def apply_3x3_filter(pixel_array, image_width, image_height, filter):
+def apply_filter(pixel_array, image_width, image_height, kernel):
     filtered_image = createInitializedGreyscalePixelArray(image_width, image_height)
+    kernel_size = len(kernel)
+    kernel_center = len(kernel) // 2
 
-    for row in range(1, image_height - 1):
-        for col in range(1, image_width - 1):
+    for row in range(kernel_center, image_height - kernel_center): ## kernel_size 3x3 (1, height - 1)
+        for col in range(kernel_center, image_width - kernel_center):
             filtered_value = 0
-            for i in range(3):
-                for j in range(3):
-                    filtered_value += pixel_array[row + i - 1][col + j - 1] * filter[i][j]
+            for i in range(kernel_size):
+                for j in range(kernel_size):
+                    filtered_value += pixel_array[row + i - kernel_center][col + j - kernel_center] * kernel[i][j]
             filtered_image[row][col] = filtered_value
     return filtered_image
 
@@ -85,19 +192,6 @@ def abs_diff(image1, image2):
     return abs_difference
 
 
-# Task 3 Image blurring
-def apply_5x5_filter(pixel_array, image_width, image_height, filter):
-    filtered_image = createInitializedGreyscalePixelArray(image_width, image_height)
-
-    for row in range(2, image_height - 2):
-        for col in range(2, image_width - 2):
-            filtered_value = 0
-            for i in range(5):
-                for j in range(5):
-                    filtered_value += pixel_array[row + i - 2][col + j - 2] * filter[i][j]
-            filtered_image[row][col] = filtered_value
-    return filtered_image
-
 # Task 4 Threshold
 def custom_threshold(image, image_width, image_height, threshold_1, threshold_2):
     for row in range(image_height):
@@ -107,6 +201,61 @@ def custom_threshold(image, image_width, image_height, threshold_1, threshold_2)
             elif image[row][col] > threshold_2:
                 image[row][col] = 255
     return image
+
+
+# Task 5
+def dilate_image(image, image_width, image_height, kernel, iterations):
+    if iterations == 0:
+        return image
+
+    # 获取 kernel 尺寸和中心点
+    k_height, k_width = len(kernel), len(kernel)
+    kernel_center = len(kernel)//2
+
+    # 创建输出图像数组，初始为零（黑色）
+    output = createInitializedGreyscalePixelArray(image_width, image_height)
+
+    # 遍历图像的每个像素
+    for row in range(kernel_center, image_height - kernel_center):
+        for col in range(kernel_center, image_width - kernel_center):
+            pixel_values = []
+            # 遍历 kernel
+            try:
+                for i in range(k_height):
+                    for j in range(k_width):
+                        if kernel[i][j] == 1 and image[row + i - kernel_center][col + j - kernel_center] != 0:
+                            raise IndexError
+                output[row][col] = 0
+            except IndexError:
+                output[row][col] = image[row][col]
+
+    # 递归进行下一次膨胀
+    return dilate_image(output, image_width, image_height, kernel, iterations - 1)
+
+def erode_image(image, image_width, image_height, kernel, iterations):
+    if iterations == 0:
+        return image
+
+    # 获取 kernel 尺寸和中心点
+    k_height, k_width = len(kernel), len(kernel[0])
+    kernel_center = len(kernel)//2
+
+    output = createInitializedGreyscalePixelArray(image_width, image_height)
+
+    # 遍历图像的每个像素
+    for row in range(kernel_center, image_height - kernel_center):
+        for col in range(kernel_center, image_width - kernel_center):
+            # 遍历 kernel
+            try:
+                for i in range(k_height):
+                    for j in range(k_width):
+                        if kernel[i][j] == 1 and image[row + i - kernel_center][col + j - kernel_center] == 0:
+                            raise IndexError
+                output[row][col] = image[row][col]
+            except IndexError:
+                output[row][col] = 0
+    return erode_image(output, image_width, image_height, kernel, iterations - 1)
+
 
 # Task 6
 class Queue:
@@ -126,13 +275,7 @@ class Queue:
         return len(self.items)
 
 def computeConnectedComponentLabeling(image, image_width, image_height):
-
-    # Add a border with value of pixel 0
-    image = [[0] * image_width] + image + [[0] * image_width]
-    for index in range(len(image)):
-        image[index] = [0] + image[index] + [0]
-
-    num = 1
+    num_region = 1
     regions = {}
 
     for row in range(1, image_height):
@@ -142,26 +285,27 @@ def computeConnectedComponentLabeling(image, image_width, image_height):
                 q.enqueue((row, col,))
                 image[row][col] = -999
                 while not q.isEmpty():
-                    (a, b,) = q.dequeue()
-                    if num not in regions.keys():
-                        regions[num] = [(a, b,)]
+                    (x_cor, y_cor) = q.dequeue()
+                    if num_region not in regions.keys():
+                        regions[num_region] = [(x_cor, y_cor)]
                     else:
-                        regions[num].append((a, b,))
+                        regions[num_region].append((x_cor, y_cor))
 
-                    if image[a - 1][b] != 0 and image[a - 1][b] != -999:
-                        q.enqueue((a - 1, b,))
-                        image[a - 1][b] = -999
-                    if image[a + 1][b] != 0 and image[a + 1][b] != -999:
-                        q.enqueue((a + 1, b,))
-                        image[a + 1][b] = -999
-                    if image[a][b + 1] != 0 and image[a][b + 1] != -999:
-                        q.enqueue((a, b + 1,))
-                        image[a][b + 1] = -999
-                    if image[a][b - 1] != 0 and image[a][b - 1] != -999:
-                        q.enqueue((a, b - 1,))
-                        image[a][b - 1] = -999
-                num += 1
+                    if image[x_cor - 1][y_cor] != 0 and image[x_cor - 1][y_cor] != -999:
+                        q.enqueue((x_cor - 1, y_cor,))
+                        image[x_cor - 1][y_cor] = -999
+                    if image[x_cor + 1][y_cor] != 0 and image[x_cor + 1][y_cor] != -999:
+                        q.enqueue((x_cor + 1, y_cor,))
+                        image[x_cor + 1][y_cor] = -999
+                    if image[x_cor][y_cor + 1] != 0 and image[x_cor][y_cor + 1] != -999:
+                        q.enqueue((x_cor, y_cor + 1,))
+                        image[x_cor][y_cor + 1] = -999
+                    if image[x_cor][y_cor - 1] != 0 and image[x_cor][y_cor - 1] != -999:
+                        q.enqueue((x_cor, y_cor - 1,))
+                        image[x_cor][y_cor - 1] = -999
+                num_region += 1
     return regions
+
 
 # Task 7
 def detect_the_region(regions):
@@ -177,11 +321,14 @@ def detect_the_region(regions):
         min_y_cor = min(y_cor)
         max_x_cor = max(x_cor)
         max_y_cor = max(y_cor)
-        if (max_x_cor - min_x_cor) > 20 and (max_y_cor - min_y_cor) > 20:
-            bounding_box_list.append((min_x_cor, min_y_cor, max_x_cor, max_y_cor))
+        # if (max_x_cor - min_x_cor) > 50 and (max_y_cor - min_y_cor) > 50 and abs((max_x_cor - min_x_cor) - (max_y_cor - min_y_cor)) < 20:
+        bounding_box_list.append((min_x_cor, min_y_cor, max_x_cor, max_y_cor))
 
 
-    print(bounding_box_list)
     print("The region has been detected from the image! ")
 
     return bounding_box_list
+
+
+
+
